284f109b9338f06cbeed762689bff52fb964616d
Michelle Corley <mcorley1@nd.edu>
2017-11-20 16:23:37 -0500

    subsetting successful

--- Ex12Q1.py
+++ Ex12Q1.py
@@ -10,5 +10,7 @@ chicken.shape
 ggplot(chicken,aes(x="feed",y="weight"))+geom_dotplot(binaxis="y",stackdir="center", stackratio=0.5, dotsize=0.2)+theme_classic()
 
 #subset data into different data frames 
-sub1=chicken.loc[data.feed.isin(:,['soybean])]
-sub2=chicken.loc[data.feed.isin(:,['sunflower])]
+sub1=chicken.loc[chicken.feed.isin(['soybean']),:]
+sub2=chicken.loc[chicken.feed.isin(['sunflower']),:]
+
+
