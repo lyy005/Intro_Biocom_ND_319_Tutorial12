8c29783dee688cf8cb79191d4adce5e34af494e5
Michelle Corley <mcorley1@nd.edu>
2017-11-20 16:12:23 -0500

    subset data - getting an error on this

--- Ex12Q1.py
+++ Ex12Q1.py
@@ -2,7 +2,13 @@ import numpy
 import pandas
 import plotnine
 from plotnine import *
+from scipy.optimize import minimize 
+from scipy.stats import norm
 
 chicken=pandas.read_csv("chickwts.txt", sep=",")
 chicken.shape
 ggplot(chicken,aes(x="feed",y="weight"))+geom_dotplot(binaxis="y",stackdir="center", stackratio=0.5, dotsize=0.2)+theme_classic()
+
+#subset data into different data frames 
+sub1=chicken.loc[data.feed.isin(:,['soybean])]
+sub2=chicken.loc[data.feed.isin(:,['sunflower])]
