ee5d955958d5b264e4bd08617e513ee18b722432
Michelle Corley <mcorley1@nd.edu>
2017-11-20 16:36:20 -0500

    attempting maximum likehood, not completed

--- Ex12Q1.py
+++ Ex12Q1.py
@@ -10,7 +10,14 @@ chicken.shape
 ggplot(chicken,aes(x="feed",y="weight"))+geom_dotplot(binaxis="y",stackdir="center", stackratio=0.5, dotsize=0.2)+theme_classic()
 
 #subset data into different data frames 
-sub1=chicken.loc[chicken.feed.isin(['soybean']),:]
-sub2=chicken.loc[chicken.feed.isin(['sunflower']),:]
+sub1=chicken.loc[chicken.feed.isin(['soybean','sunflower']),:]
+
+#Make new data frame with 'group' column (your x=0 or x=1)
+#var2=pandas.DataFrame({'y':var1.col2name, 'x':})
+sub1frame=pandas.DataFrame({'y':sub1.feed,'x':0})
+
+#Designate 'treatment' group as x=1
+#var2.loc[var1.col1name=='name of treatment group', 'x']=1
+sub1frame.loc[sub1.weight=='sunflower','x']=1
 
 
